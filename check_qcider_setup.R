ac89d4bcccfb1eea9f1977499e3303cbd2a4f624
Andrew Lockhart <48281805+lockhartandrewj@users.noreply.github.com>
2019-04-24 07:58:34 +1000

    Initial edit

---
+++ check_qcider_setup.R
@@ -0,0 +1,15 @@
+# Checks whether qcider package data connections are setup correctly
+
+# can be called separately and inside getConnection functions
+
+# when called separately it can have the parameter verbose set to TRUE
+
+
+
+# First check if h:\user.dat exists
+
+# Then check each connection in the user.dat file.
+# -  If verbose = TRUE then provide useful human readable feedback
+
+# Return TRUE if file exists and NoCS and CDU_BI_RO are set up and functioning correctly, otherwise FALSE.
+
